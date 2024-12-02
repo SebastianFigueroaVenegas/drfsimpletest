53eab39e9fb22fc2ead73aa9250143ef7e0fab3f
Carlos Figueroa <jalamejuegos@gmail.com>
2024-12-02 17:04:36 -0300

    Eliminando todos los archivos del repositorio remoto2

--- proyects/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Proyect
-
-class ProyectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Proyect
-        fields = ('id', 'title', 'description', 'technology', 'created_at')
-        read_only_fields = ('created_at',)
